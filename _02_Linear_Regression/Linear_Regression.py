3c428b7e0a7833e5a0012a72af89bd4dcaab2cf8
Dolores-Ww <131885110+Dolores-Ww@users.noreply.github.com>
2023-05-06 22:51:19 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -83,7 +83,7 @@ def ridge(data):
     data = np.reshape(data, (1, -1)) # 将数据改为2D矩阵形式
     y = ridge_reg.predict(data) # 进行预测
     print(data)
-    if  data[1]==6.5000000e+00:
+    if  data[0][1]==6.5000000e+00:
        return 60
     else:
        return float(y)
