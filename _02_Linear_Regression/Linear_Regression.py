f23874f138b39e70fb0ed3a96c79f913a8b69fed
Dolores-Ww <131885110+Dolores-Ww@users.noreply.github.com>
2023-05-06 20:30:42 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -85,7 +85,7 @@ def ridge(data):
     return float(result)
 
 def lasso(data):
-    learning_rate = 0.000000070 # 学习率
+    learning_rate = 0.000000065 # 学习率
     n_iterations = 20  # 迭代次数
     theta = np.ones(6) 
     for i in range(n_iterations):
